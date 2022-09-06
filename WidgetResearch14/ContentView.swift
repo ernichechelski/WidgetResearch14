fd0e0510ae48417df1a37296c33e348838c3e605
Ernest Chechelski <ernichechelski@gmail.com>
2022-09-06 10:28:20 +0200

    Initial Commit

---
+++ WidgetResearch14/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WidgetResearch14
+//
+//  Created by Ernest Chechelski on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetResearch14/WidgetResearch14App.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetResearch14App.swift
+//  WidgetResearch14
+//
+//  Created by Ernest Chechelski on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WidgetResearch14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
